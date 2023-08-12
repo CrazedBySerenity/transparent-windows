4501c52161e3bc842edf5ba22d95f6dba26db69e
CrazedBySerenity <villeisakd@gmail.com>
2023-08-12 16:30:56 +0200

    Added support for keys 1-9 to be used for different levels of transparency. Added the requirement of the shift key to be pressed simultaneously to change transparency levels

--- transparency.py
+++ transparency.py
@@ -18,11 +18,48 @@ def changeWindowOpacity():
 
 while True:
     event = keyboard.read_event()
-    if event.name == '1' and event.event_type == 'down':
-        opacity = opacity_step_1
-        changeWindowOpacity()
-        print('made transparent')
-    if event.name == '9' and event.event_type == 'down':
-        opacity = opacity_step_10
-        changeWindowOpacity()
-        print('made opaque')
+    if keyboard.is_pressed('shift'):
+        if keyboard.is_pressed('1') and event.event_type == 'down':
+            opacity = opacity_step_1
+            changeWindowOpacity()
+            print('made transparent')
+
+        if keyboard.is_pressed('2') and event.event_type == 'down':
+            opacity = round(((opacity_step_10 - opacity_step_1) / 8 )* 2)
+            changeWindowOpacity()
+            print('updated transparency')
+
+        if keyboard.is_pressed('3')  and event.event_type == 'down':
+            opacity = round(((opacity_step_10 - opacity_step_1) / 8 )* 3)
+            changeWindowOpacity()
+            print('updated transparency')
+
+        if keyboard.is_pressed('4') and event.event_type == 'down':
+            opacity = round(((opacity_step_10 - opacity_step_1) / 8 )* 4)
+            changeWindowOpacity()
+            print('updated transparency')
+
+        if keyboard.is_pressed('5') and event.event_type == 'down':
+            opacity = round(((opacity_step_10 - opacity_step_1) / 8 )* 5)
+            changeWindowOpacity()
+            print('updated transparency')
+
+        if keyboard.is_pressed('6') and event.event_type == 'down':
+            opacity = round(((opacity_step_10 - opacity_step_1) / 8 )* 6)
+            changeWindowOpacity()
+            print('updated transparency')
+
+        if keyboard.is_pressed('7') and event.event_type == 'down':
+            opacity = round(((opacity_step_10 - opacity_step_1) / 8 )* 7)
+            changeWindowOpacity()
+            print('updated transparency')
+
+        if keyboard.is_pressed('8') and event.event_type == 'down':
+            opacity = round(((opacity_step_10 - opacity_step_1) / 8 )* 8)
+            changeWindowOpacity()
+            print('updated transparency')
+
+        if keyboard.is_pressed('9') and event.event_type == 'down':
+            opacity = opacity_step_10
+            changeWindowOpacity()
+            print('made opaque')
